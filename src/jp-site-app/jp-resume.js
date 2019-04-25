e40a43bec877dc154d8374faaaf78252cd9bef2e
Justin Platz <jmplatz@us.ibm.com>
2019-04-25 08:42:31 -0400

    Updates

--- src/jp-site-app/jp-resume.js
+++ src/jp-site-app/jp-resume.js
@@ -23,7 +23,7 @@ define(["../../node_modules/@polymer/polymer/polymer-element.js"],function(_poly
 
       <!-- <canvas id="canvas"></canvas> -->
 
-      <object class="pdf" data="css/images/JustinPlatzResume2018.pdf" type="application/pdf"/>
+      <object class="pdf" data="css/images/JustinPlatzResume2019.pdf" type="application/pdf"/>
         <iframe class="pdf" src="https://drive.google.com/file/d/1Sa49KoTryUjGStI7QB_Uzc1pliRo0dLv/preview"></iframe>
       </object>
 
